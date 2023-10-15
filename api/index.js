717a3c414b4f893635ebc6dde7c0c33a8cdfb8b2
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-15 11:10:15 +0000

    Connect To database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
+// Connect to the MongoDB database
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    
+    // Set up your routes and middleware here
+    
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
