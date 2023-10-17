e1e2a6047b01137ffcd692fdfd4f754744aa4635
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-17 05:52:03 +0000

    Updated the home page with some css and also added some new features to the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-slate-200 shadow-md fixed top-0 left-0 right-0 z-10'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -13,33 +13,33 @@ export default function Header() {
             <span className='text-slate-700'>Farms</span>
           </h1>
         </Link>
-        {/* <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input
-            type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
-          />
-          <FaSearch className='text-slate-600' />
-        </form> */}
-        <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              Home
-            </li>
-          </Link>
+        <ul className='flex gap-4 items-center'>
+          {!currentUser && (
+            <Link to='/signup'>
+              <li className='hidden sm:inline text-slate-700 hover:underline'>
+                SignUp
+              </li>
+            </Link>
+          )}
           <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
           </Link>
-          <Link to="/profile">
+          <Link to="/profile" className="flex items-center">
             {currentUser ? (
-              <img src={currentUser.avatar} alt="profile" className="rounded-full h-8 w-8" />
+              <>
+                <img
+                  src={currentUser.avatar}
+                  alt="profile"
+                  className="rounded-full h-8 w-8 mr-2"
+                />
+                <li className="text-slate-700 hover:underline">SignOut</li>
+              </>
             ) : (
               <li className="text-slate-700 hover:underline">Sign in</li>
             )}
           </Link>
-
         </ul>
       </div>
     </header>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,211 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../styles/style.css';
 
-export default function Home() {
+const HomePage = () => {
+  // Assuming you have a Redux state to get user information
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>
-      Home
+    <div className="flex h-screen bg-black pt-16">
+      <aside className="aside-menu bg-gradient-to-b from-blue-500 to-blue-700 text-white w-96 h-screen mb-8 pb-4 overflow-y-auto pl-4 ml-4 pt-16">
+        <h2 className="text-2xl font-bold mb-8">Cocoa Business</h2>
+        <ul className="space-y-4">
+          {/* Dashboard Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Dashboard</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/dashboard" className="text-white hover:underline">
+                  Overview
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* Inventory Management Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Inventory Management</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/receive-cocoa" className="text-white hover:underline">
+                  Receive Cocoa Bags
+                </Link>
+              </li>
+              <li>
+                <Link to="/manage-inventory" className="text-white hover:underline">
+                  Manage Inventory
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* Sales Management Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Sales Management</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/create-invoice" className="text-white hover:underline">
+                  Create Invoice
+                </Link>
+              </li>
+              <li>
+                <Link to="/manage-orders" className="text-white hover:underline">
+                  Manage Orders
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* Financial Management Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Financial Management</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/record-income" className="text-white hover:underline">
+                  Income
+                </Link>
+              </li>
+              <li>
+                <Link to="/record-expenditures" className="text-white hover:underline">
+                  Expenditures
+                </Link>
+              </li>
+              <li>
+                <Link to="/access-reports" className="text-white hover:underline">
+                  Reports
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* Reports & Analytics Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Reports & Analytics</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/sales-reports" className="text-white hover:underline">
+                  Sales Reports
+                </Link>
+              </li>
+              <li>
+                <Link to="/financial-reports" className="text-white hover:underline">
+                  Financial Reports
+                </Link>
+              </li>
+              <li>
+                <Link to="/inventory-reports" className="text-white hover:underline">
+                  Inventory Reports
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* Settings Section */}
+          <li>
+            <h3 className="text-lg font-semibold mb-2">Settings</h3>
+            <ul className="ml-4 space-y-2">
+              <li>
+                <Link to="/profile" className="text-white hover:underline">
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="/business-settings" className="text-white hover:underline">
+                  Business Settings
+                </Link>
+              </li>
+              <li>
+                <Link to="/manage-users" className="text-white hover:underline">
+                  Users
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </aside>
+      {/* Content */}
+      <div className="flex flex-wrap flex-grow p-8 space-x-4">
+        {/* Welcome Section */}
+        <section className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-8 w-full md:w-1/2 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">
+            Welcome to Cocoa Warehouse, {currentUser ? currentUser.username : 'Guest'}!
+          </h2>
+          <p className="text-lg">
+            Greet the user and provide a brief overview of the business and its mission. The mission of Pador Farms is to grow to be a major cocoa buying company at the district, regional, national, and global stages. We aim to provide high-quality cocoa products and contribute to the sustainable growth of the cocoa industry.
+          </p>
+        </section>
+        {/* Quick Actions */}
+        <section className="bg-gradient-to-r from-green-400 to-blue-500 text-white p-8 w-full md:w-1/2 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Quick Actions</h2>
+          <div className="flex flex-wrap gap-4">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+              Add New Inventory
+            </button>
+            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">
+              Create Invoice
+            </button>
+            <button className="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-700">
+              View Reports
+            </button>
+          </div>
+        </section>
+        {/* Statistics Overview */}
+        <section className="bg-gradient-to-r from-yellow-400 to-orange-500 text-white p-8 w-full md:w-1/3 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Statistics Overview</h2>
+          {/* Display key statistics such as total cocoa bags, total sales, current revenue, recent transactions, etc. */}
+          {/* Example: */}
+          <p>Total Cocoa Bags: 1000</p>
+          <p>Total Sales: $5000</p>
+          {/* Add more statistics here */}
+        </section>
+        {/* Inventory Summary */}
+        <section className="bg-gradient-to-r from-pink-400 to-red-500 text-white p-8 w-full md:w-1/3 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Inventory Summary</h2>
+          {/* Show a summary of current inventory status including quantity, quality, and expiration dates. */}
+          {/* Example: */}
+          <p>Quantity: 500 bags</p>
+          <p>Quality: Premium Grade</p>
+          {/* Add more inventory details here */}
+        </section>
+        {/* Recent Transactions */}
+        <section className="bg-gradient-to-r from-purple-400 to-pink-500 text-white p-8 w-full md:w-1/3 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Recent Transactions</h2>
+          {/* Display a list of recent transactions including new inventory received and customer orders fulfilled. */}
+          {/* Example: */}
+          <ul>
+            <li>New Inventory Received - 50 bags</li>
+            <li>Order Fulfilled - Customer Name, Date</li>
+            {/* Add more transaction details here */}
+          </ul>
+        </section>
+        {/* Financial Snapshot */}
+        <section className="bg-gradient-to-r from-teal-400 to-cyan-500 text-white p-8 w-full md:w-1/2 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Financial Snapshot</h2>
+          {/* Present an overview of the financial status including income, expenditures, and overall profit/loss. */}
+          {/* Example: */}
+          <p>Income: $7000</p>
+          <p>Expenditures: $2000</p>
+          <p>Profit/Loss: $5000</p>
+          {/* Add more financial details here */}
+        </section>
+        {/* Upcoming Tasks */}
+        <section className="bg-gradient-to-r from-yellow-500 to-green-500 text-white p-8 w-full md:w-1/2 mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Upcoming Tasks</h2>
+          {/* Highlight upcoming tasks such as scheduled deliveries, pending invoices, or low inventory alerts. */}
+          {/* Example: */}
+          <ul>
+            <li>Scheduled Deliveries: Tomorrow, Customer Name</li>
+            <li>Pending Invoices: 2 invoices to be sent</li>
+            {/* Add more upcoming tasks here */}
+          </ul>
+        </section>
+        {/* Graphs and Charts */}
+        <section className="bg-gradient-to-r from-orange-400 to-yellow-500 text-white p-8 w-full mb-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Graphs and Charts</h2>
+          {/* Display graphs and charts to visualize data (e.g., sales trends, inventory levels over time, financial performance). */}
+          {/* Example: */}
+          {/* You can use charting libraries like Chart.js or D3.js to create interactive graphs */}
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default HomePage;
